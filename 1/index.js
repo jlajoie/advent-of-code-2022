94a9b5fe2a4e7562420dbc1c2bec5bf8ce3f8334
Jeff LaJoie <jeff.lajoie@zenlist.com>
2022-12-01 10:52:23 -0500

    advent of code 2022 - day 1

---
+++ 1/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+let input = fs
+  .readFileSync(`${__dirname}/input.txt`)
+  .toString()
+  .split("\n\n")
+  .map((chunk) => chunk.split("\n").map((line) => parseInt(line)));
+
+// Part One
+let partOneOutput = Math.max(
+  ...input.map((chunk) => chunk.reduce((sum, a) => sum + a, 0))
+);
+console.dir(partOneOutput);
+
+// Part Two
+let partTwoOutput = input
+  .map((chunk) => chunk.reduce((sum, a) => sum + a, 0))
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((sum, a) => sum + a, 0);
+console.dir(partTwoOutput);
